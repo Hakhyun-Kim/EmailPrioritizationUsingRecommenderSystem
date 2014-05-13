64e897f90e77abff2eeee627083b97eb03201751
Harry Kim <kimhakhyun@gmail.com>
2014-05-13 22:30:39 +0900

    Added. Email count

--- IMAP_Parser.py
+++ IMAP_Parser.py
@@ -5,13 +5,16 @@ import sys
 import email
 import re
 
+from collections import defaultdict
+
 #FOLDER=sys.argv[1]
 FOLDER='Inbox'
 LOGIN='your name@sunykorea.ac.kr'
 PASSWORD='your password'
 IMAP_HOST = 'imap.gmail.com'  # Change this according to your provider
 
-email_list = []
+email_senderDict = defaultdict(int)
+email_receiverDict = defaultdict(int)
 email_unique = []
 
 mail = imaplib.IMAP4_SSL(IMAP_HOST)
@@ -27,8 +30,27 @@ for i in id_list:
 		if isinstance(response_part, tuple):
 			msg = email.message_from_string(response_part[1])
 			sender = msg['from'].split()[-1]
-			address = re.sub(r'[<>]','',sender)
-# Ignore any occurences of own email address and add to list
-	if not re.search(r'' + re.escape(LOGIN),address) and not address in email_list:
-		email_list.append(address)
-		print address
+			receiver = msg['to'].split()[-1]
+			sender_address = re.sub(r'[<>]','',sender)
+			receiver_address = re.sub(r'[<>]','',receiver)
+			
+	#print 'sender: ' + sender_address
+	#print 'receiver: ' + receiver_address 
+
+	if sender_address in email_senderDict:
+		email_senderDict[sender_address] += 1
+	else:
+		email_senderDict[sender_address] = 1
+
+	if receiver_address in email_receiverDict:
+		email_receiverDict[receiver_address] += 1
+	else:
+		email_receiverDict[receiver_address] = 1
+
+print 'send list'
+for key, value in email_senderDict.iteritems():
+	print 'to', key, value, 'emails sent'
+
+print 'receiver list'
+for key, value in email_receiverDict.iteritems():
+	print 'from', key, value, 'emails received'
