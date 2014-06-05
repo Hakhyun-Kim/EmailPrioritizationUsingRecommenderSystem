16ec104533d1f24abcff02e38defbb38b86c700d
Harry Kim <kimhakhyun@gmail.com>
2014-06-05 23:48:28 +0900

    Added. login sessin for multiple email address

--- IMAP_Parser.py
+++ IMAP_Parser.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 
 #FOLDER=sys.argv[1]
 FOLDER='Inbox'
-LOGIN='your name@sunykorea.ac.kr'
+LOGINDATA = {'your email',}
 PASSWORD='your password'
 IMAP_HOST = 'imap.gmail.com'  # Change this according to your provider
 
@@ -17,47 +17,56 @@ email_senderDict = defaultdict(int)
 email_receiverDict = defaultdict(int)
 email_unique = []
 
-mail = imaplib.IMAP4_SSL(IMAP_HOST)
-mail.login(LOGIN, PASSWORD)
-mail.select(FOLDER) 
 
-result, data = mail.search(None, 'ALL')
-ids = data[0]
-id_list = ids.split()
-for i in id_list:
-	typ, data = mail.fetch(i,'(RFC822)')
-	for response_part in data:
-		if isinstance(response_part, tuple):
-			msg = email.message_from_string(response_part[1])
-			sender = msg['from'].split()[-1]
-			receiver = msg['to'].split()[-1]
-			sender_address = re.sub(r'[<>]','',sender)
-			receiver_address = re.sub(r'[<>]','',receiver)
+for LOGIN in LOGINDATA:
+	
+	mail = imaplib.IMAP4_SSL(IMAP_HOST)
+	mail.login(LOGIN, PASSWORD)
+	mail.select(FOLDER) 
+
+	result, data = mail.search(None, 'ALL')
+	ids = data[0]
+	id_list = ids.split()
+	for i in id_list:
+		typ, data = mail.fetch(i,'(RFC822)')
+		for response_part in data:
+			if isinstance(response_part, tuple):
+				msg = email.message_from_string(response_part[1])
+				
+				if(type(msg['from']) != type(None)):
+					sender = msg['from'].split()[-1]
+				if(type(msg['to']) != type(None)):
+					receiver = msg['to'].split()[-1]
+					sender_address = re.sub(r'[<>]','',sender)
+					receiver_address = re.sub(r'[<>]','',receiver)
 			
-	#print 'sender: ' + sender_address
-	#print 'receiver: ' + receiver_address 
+		#print 'sender: ' + sender_address
+		#print 'receiver: ' + receiver_address 
 
-	if sender_address in email_senderDict:
-		email_senderDict[sender_address] += 1
-	else:
-		email_senderDict[sender_address] = 1
+		if sender_address in email_senderDict:
+			email_senderDict[sender_address] += 1
+		else:
+			email_senderDict[sender_address] = 1
 
-	if receiver_address in email_receiverDict:
-		email_receiverDict[receiver_address] += 1
-	else:
-		email_receiverDict[receiver_address] = 1
+		if receiver_address in email_receiverDict:
+			email_receiverDict[receiver_address] += 1
+		else:
+			email_receiverDict[receiver_address] = 1
 
-#print 'sender list'
-#for key, value in email_senderDict.iteritems():
-#	print 'to', key, value, 'emails sent'
+	#mail.close()
+	#mail.shutdown()
+	#print 'sender list'
+	#for key, value in email_senderDict.iteritems():
+	#	print 'to', key, value, 'emails sent'
 
-#print 'receiver list'
-#for key, value in email_receiverDict.iteritems():
-#	print 'from', key, value, 'emails received'
+	#print 'receiver list'
+	#for key, value in email_receiverDict.iteritems():
+	#	print 'from', key, value, 'emails received'
 
 print 'match list'
 for key, value in email_senderDict.iteritems():
     if key in email_receiverDict:
         print key,'send count', value, 'receive count', email_receiverDict[key]
 
- 
+def utility_emailexchnage(sendcount, receivercount):
+		return (sendcount-receivercount)*(sendcount-receivercount);
